351f41529400ad94f9dad32203d1aa294eb0d9b1
P. Envall <petter.envall@gmail.com>
2013-01-29 19:52:58 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+// Instantiation
+var express = require("express")
+  , app = express();
+
+// Setup
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.set("views", __dirname + "/views");
+  app.set("view engine", "jade");
+  app.use(require("stylus").middleware({ src: __dirname + "/public" }));
+  app.use(express["static"](__dirname + "/public"));
+});
+
+// View helpers
+app.locals({
+  "bodyClass": "unknown"
+  , "nav": "unknown"
+  , "util":  {
+    "foo": 42
+  }
+  , "cssAll": ["base.css"]
+  , "cssCustom": []
+});
+
+
+// Routes
+app.get("/", function (req, res) {
+  res.render("index", {
+    "pt": "Home"
+    , "nav": "home"
+    , "cssCustom": []
+  });
+});
+
+
+app.get("/maps", function (req, res) {
+  res.render("maps", {
+    "pt": "Maps"
+    , "nav": "maps"
+    , "cssCustom": []
+  });
+});
+
+
+
+// Run application
+app.listen(3000);
